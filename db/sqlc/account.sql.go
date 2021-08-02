fd3c76b49914540aca8c6a246ef190daeaa2e349
Juno <shy12@case.edu>
2021-08-03 03:10:27 +0900

    CRUD for entries and transfers

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -7,7 +7,6 @@ import (
 	"context"
 )
 
-// Create
 const createAccount = `-- name: CreateAccount :one
 INSERT INTO accounts (
   owner,
@@ -38,7 +37,16 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 	return i, err
 }
 
-// Read
+const deleteAccount = `-- name: DeleteAccount :exec
+DELETE FROM accounts 
+WHERE id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
+}
+
 const getAccount = `-- name: GetAccount :one
 SELECT id, owner, balance, currency, created_at FROM accounts
 WHERE id = $1 LIMIT 1
@@ -98,8 +106,7 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 	return items, nil
 }
 
-// Update
-const updateAccount = `-- name: UpdateAccount :exec
+const updateAccount = `-- name: UpdateAccount :one
 UPDATE accounts 
 SET balance = $2
 WHERE id = $1
@@ -111,18 +118,15 @@ type UpdateAccountParams struct {
 	Balance int64 `json:"balance"`
 }
 
-func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) error {
-	_, err := q.db.ExecContext(ctx, updateAccount, arg.ID, arg.Balance)
-	return err
-}
-
-// Delete
-const deleteAccount = `-- name: DeleteAccount :exec
-DELETE FROM accounts 
-WHERE id = $1
-`
-
-func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
-	_, err := q.db.ExecContext(ctx, deleteAccount, id)
-	return err
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
 }
